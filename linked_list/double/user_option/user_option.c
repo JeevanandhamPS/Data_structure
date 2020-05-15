d698ad456f066cbd4c7e06504c107ad25cdcb271
P . S . JEEVA11 <42694368+JeevanandhamPS@users.noreply.github.com>
2020-05-15 09:18:10 +0530

    double_linked_list with user option

---
+++ linked_list/double/user_option/user_option.c
@@ -0,0 +1,106 @@
+/***************************************/
+/* Author	: Jeevanandham P S     */
+/* Date		: 15-05-2020           */
+/* Filename	: user_option.c	       */
+/* Description	: Double linked list   */
+/***************************************/
+
+#include "user_option.h"
+
+struct database *head = NULL;
+
+int main()
+{
+	int option;
+
+
+	while(1){
+		printf("Please enter your option\n1.Add New Data\n2.Print Database\n3.exit\n");
+		scanf("%d",&option);
+	
+		switch (option){
+			case 1:
+				add();
+				break;
+			case 2:
+				print();
+				break;
+			case 3:
+				exit(0);
+			default:
+				printf("Invalid option\n");
+				break;
+		}
+	}
+}
+
+void add()
+{
+	int ret;
+
+	struct database *ptr = head;
+
+	struct database *temp = (struct database *)malloc(sizeof(struct database));
+
+	printf("Enter details\n");
+	scanf("%s",temp->name);	
+
+	temp->next = NULL;
+	temp->prev = NULL;
+
+	if(head == NULL){
+		head = temp;
+	}
+	else{
+		while(ptr != NULL)
+		{
+			ret = strcmp(temp->name, ptr->name);
+
+			printf("%d\n",ret);
+			if(ret == 0){
+				printf("Member already exist\n");
+				break;
+			}
+			else if( ret > 0) {
+				if(ptr->next == NULL){
+					ptr->next = temp;
+					temp->prev = ptr;
+					break;
+				}
+				else{
+					ptr = ptr->next;
+				}
+			}
+			else if(ret < 0){
+				if(ptr == head){
+					temp->next = head;
+					head->prev = temp;
+					head = temp;
+					break;
+				}
+				else{
+					ptr->prev = temp;
+					temp->next = ptr;
+					break;
+				}
+			}
+		}
+	}
+
+}
+
+void print()
+{
+	struct database *print_ptr = head;
+
+	if(head == NULL){
+		printf("List is Empty\n");
+		return;
+	}
+
+	while(print_ptr != NULL){
+		printf("%s\n",print_ptr->name);
+		print_ptr = print_ptr->next;
+	}
+	return;
+}
