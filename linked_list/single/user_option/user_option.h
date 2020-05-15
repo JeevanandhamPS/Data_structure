82e1cf35f6c178d9ca1e49c226c5145e71c5f3e0
P . S . JEEVA11 <42694368+JeevanandhamPS@users.noreply.github.com>
2020-05-15 09:09:42 +0530

    Create user_option.h

---
+++ linked_list/single/user_option/user_option.h
@@ -0,0 +1,32 @@
+/***************************************/
+/* Author	: Jeevanandham P S     */
+/* Date		: 15-05-2020           */
+/* Filename	: user_option.h	       */
+/* Description	: header files         */
+/***************************************/
+
+#ifndef USER_OPTION_H
+#define USER_OPTION_H
+
+/* Included header files */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+/* function prototypes */
+
+void add();
+void print();
+
+/* user defined datatype definitions */
+
+struct database{
+	int roll_num;
+	char name[10];
+	struct database *next;
+};
+
+#endif //USER_OPTION_H
